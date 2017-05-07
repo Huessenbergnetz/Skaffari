1acb49db62bca9bb810b275651113f57caf128fe
Buschmann <kontakt@buschmann23.de>
2017-05-07 19:32:25 +0200

    Move configuration to central SkaffariConfig class

---
+++ src/utils/skaffariconfig.cpp
@@ -0,0 +1,103 @@
+/*
+ * Skaffari - a mail account administration web interface based on Cutelyst
+ * Copyright (C) 2017 Matthias Fehring <kontakt@buschmann23.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "skaffariconfig.h"
+#include "../common/config.h"
+
+Password::Type SkaffariConfig::m_accPwType = static_cast<Password::Type>(SK_DEF_ACC_PWTYPE);
+Password::Method SkaffariConfig::m_accPwMethod = static_cast<Password::Method>(SK_DEF_ACC_PWMETHOD);
+quint32 SkaffariConfig::m_accPwRounds = SK_DEF_ACC_PWROUNDS;
+quint8 SkaffariConfig::m_accPwMinlength = SK_DEF_ACC_PWMINLENGTH;
+
+QCryptographicHash::Algorithm SkaffariConfig::m_admPwMethod = static_cast<QCryptographicHash::Algorithm>(SK_DEF_ADM_PWMETHOD);
+quint32 SkaffariConfig::m_admPwRounds = SK_DEF_ADM_PWROUNDS;
+quint8 SkaffariConfig::m_admPwMinlength = SK_DEF_ADM_PWMINLENGTH;
+
+quint32 SkaffariConfig::m_defDomainquota = SK_DEF_DEF_DOMAINQUOTA;
+quint32 SkaffariConfig::m_defQuota = SK_DEF_DEF_QUOTA;
+quint32 SkaffariConfig::m_defMaxaccounts = SK_DEF_DEF_MAXACCOUNTS;
+QString SkaffariConfig::m_defLanguage = QLatin1String(SK_DEF_DEF_LANGUAGE);
+QString SkaffariConfig::m_defTimezone = QLatin1String(SK_DEF_DEF_TIMEZONE);
+
+QString SkaffariConfig::m_imapHost;
+QString SkaffariConfig::m_imapUser;
+QString SkaffariConfig::m_imapPassword;
+quint16 SkaffariConfig::m_imapPort = 143;
+QAbstractSocket::NetworkLayerProtocol SkaffariConfig::m_imapProtocol = static_cast<QAbstractSocket::NetworkLayerProtocol>(SK_DEF_IMAP_PROTOCOL);
+SkaffariIMAP::EncryptionType SkaffariConfig::m_imapEncryption = static_cast<SkaffariIMAP::EncryptionType>(SK_DEF_IMAP_ENCRYPTION);
+Account::CreateMailbox SkaffariConfig::m_imapCreatemailbox = static_cast<Account::CreateMailbox>(SK_DEF_IMAP_CREATEMAILBOX);
+bool SkaffariConfig::m_imapDomainasprefix = SK_DEF_IMAP_DOMAINASPREFIX;
+bool SkaffariConfig::m_imapFqun = SK_DEF_IMAP_FQUN;
+
+SkaffariConfig::SkaffariConfig()
+{
+
+}
+
+void SkaffariConfig::load(const QVariantMap &accounts, const QVariantMap &admins, const QVariantMap &defaults, const QVariantMap &imap)
+{
+    SkaffariConfig::m_accPwType = static_cast<Password::Type>(accounts.value(QStringLiteral("pwtype"), SK_DEF_ACC_PWTYPE).value<quint8>());
+    SkaffariConfig::m_accPwMethod = static_cast<Password::Method>(accounts.value(QStringLiteral("pwmethod"), SK_DEF_ACC_PWMETHOD).value<quint8>());
+    SkaffariConfig::m_accPwRounds = accounts.value(QStringLiteral("pwrounds"), SK_DEF_ACC_PWROUNDS).value<quint32>();
+    SkaffariConfig::m_accPwMinlength = accounts.value(QStringLiteral("pwminlength"), SK_DEF_ACC_PWMINLENGTH).value<quint8>();
+
+    SkaffariConfig::m_admPwMethod = static_cast<QCryptographicHash::Algorithm>(admins.value(QStringLiteral("pwmethod"), SK_DEF_ADM_PWMETHOD).value<quint8>());
+    SkaffariConfig::m_admPwRounds = admins.value(QStringLiteral("pwrounds"), SK_DEF_ADM_PWROUNDS).value<quint32>();
+    SkaffariConfig::m_admPwMinlength = admins.value(QStringLiteral("pwminlength"), SK_DEF_ADM_PWMINLENGTH).value<quint8>();
+
+    SkaffariConfig::m_defDomainquota = defaults.value(QStringLiteral("domainquota"), SK_DEF_DEF_DOMAINQUOTA).value<quint32>();
+    SkaffariConfig::m_defQuota = defaults.value(QStringLiteral("quota"), SK_DEF_DEF_QUOTA).value<quint32>();
+    SkaffariConfig::m_defMaxaccounts = defaults.value(QStringLiteral("maxaccounts"), SK_DEF_DEF_MAXACCOUNTS).value<quint32>();
+    SkaffariConfig::m_defLanguage = defaults.value(QStringLiteral("language"), QLatin1String(SK_DEF_DEF_LANGUAGE)).toString();
+    SkaffariConfig::m_defTimezone = defaults.value(QStringLiteral("timezone"), QLatin1String(SK_DEF_DEF_TIMEZONE)).toString();
+
+    SkaffariConfig::m_imapHost = imap.value(QStringLiteral("host")).toString();
+    SkaffariConfig::m_imapUser = imap.value(QStringLiteral("user")).toString();
+    SkaffariConfig::m_imapPassword = imap.value(QStringLiteral("password")).toString();
+    SkaffariConfig::m_imapPort = imap.value(QStringLiteral("port"), 143).value<quint16>();
+    SkaffariConfig::m_imapProtocol = static_cast<QAbstractSocket::NetworkLayerProtocol>(imap.value(QStringLiteral("protocol"), SK_DEF_IMAP_PROTOCOL).value<quint8>());
+    SkaffariConfig::m_imapEncryption = static_cast<SkaffariIMAP::EncryptionType>(imap.value(QStringLiteral("encryption"), SK_DEF_IMAP_ENCRYPTION).value<quint8>());
+    SkaffariConfig::m_imapCreatemailbox = static_cast<Account::CreateMailbox>(imap.value(QStringLiteral("createmailbox"), SK_DEF_IMAP_CREATEMAILBOX).value<quint8>());
+    SkaffariConfig::m_imapDomainasprefix = imap.value(QStringLiteral("domainasprefix"), SK_DEF_IMAP_DOMAINASPREFIX).toBool();
+    SkaffariConfig::m_imapFqun = imap.value(QStringLiteral("fqun"), SK_DEF_IMAP_FQUN).toBool();
+}
+
+Password::Type SkaffariConfig::accPwType() { return m_accPwType; }
+Password::Method SkaffariConfig::accPwMethod() { return m_accPwMethod; }
+quint32 SkaffariConfig::accPwRounds() { return m_accPwRounds; }
+quint8 SkaffariConfig::accPwMinlength() { return m_accPwMinlength; }
+
+QCryptographicHash::Algorithm SkaffariConfig::admPwMethod() { return m_admPwMethod; }
+quint32 SkaffariConfig::admPwRounds() { return m_admPwRounds; }
+quint8 SkaffariConfig::admPwMinlength() { return m_admPwMinlength; }
+
+quint32 SkaffariConfig::defDomainquota() { return SkaffariConfig::m_defDomainquota; }
+quint32 SkaffariConfig::defQuota() { return SkaffariConfig::m_defQuota; }
+quint32 SkaffariConfig::defMaxaccounts() { return SkaffariConfig::m_defMaxaccounts; }
+QString SkaffariConfig::defLanguage() { return SkaffariConfig::m_defLanguage; }
+QString SkaffariConfig::defTimezone() { return SkaffariConfig::m_defTimezone; }
+
+QString SkaffariConfig::imapHost() { return SkaffariConfig::m_imapHost; }
+quint16 SkaffariConfig::imapPort() { return SkaffariConfig::m_imapPort; }
+QString SkaffariConfig::imapUser() { return SkaffariConfig::m_imapUser; }
+QString SkaffariConfig::imapPassword() { return SkaffariConfig::m_imapPassword; }
+QAbstractSocket::NetworkLayerProtocol SkaffariConfig::imapProtocol() { return SkaffariConfig::m_imapProtocol; }
+SkaffariIMAP::EncryptionType SkaffariConfig::imapEncryption() { return SkaffariConfig::m_imapEncryption; }
+Account::CreateMailbox SkaffariConfig::imapCreatemailbox() { return SkaffariConfig::m_imapCreatemailbox; }
+bool SkaffariConfig::imapDomainasprefix() { return SkaffariConfig::m_imapDomainasprefix; }
+bool SkaffariConfig::imapFqun() { return SkaffariConfig::m_imapFqun; }
---
+++ src/utils/skaffariconfig.h
@@ -0,0 +1,89 @@
+/*
+ * Skaffari - a mail account administration web interface based on Cutelyst
+ * Copyright (C) 2017 Matthias Fehring <kontakt@buschmann23.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SKAFFARICONFIG_H
+#define SKAFFARICONFIG_H
+
+#include <QCryptographicHash>
+#include <QAbstractSocket>
+#include "../../common/password.h"
+#include "../imap/skaffariimap.h"
+#include "../objects/account.h"
+
+class SkaffariConfig
+{
+public:
+    SkaffariConfig();
+
+    static void load(const QVariantMap &accounts, const QVariantMap &admins, const QVariantMap &defaults, const QVariantMap &imap);
+
+    static bool isInitialized();
+
+    static Password::Type accPwType();
+    static Password::Method accPwMethod();
+    static quint32 accPwRounds();
+    static quint8 accPwMinlength();
+
+    static QCryptographicHash::Algorithm admPwMethod();
+    static quint32 admPwRounds();
+    static quint8 admPwMinlength();
+
+    static quint32 defDomainquota();
+    static quint32 defQuota();
+    static quint32 defMaxaccounts();
+    static QString defLanguage();
+    static QString defTimezone();
+
+    static QString imapHost();
+    static quint16 imapPort();
+    static QString imapUser();
+    static QString imapPassword();
+    static QAbstractSocket::NetworkLayerProtocol imapProtocol();
+    static SkaffariIMAP::EncryptionType imapEncryption();
+    static Account::CreateMailbox imapCreatemailbox();
+    static bool imapDomainasprefix();
+    static bool imapFqun();
+
+private:
+    static Password::Type m_accPwType;
+    static Password::Method m_accPwMethod;
+    static quint32 m_accPwRounds;
+    static quint8 m_accPwMinlength;
+
+    static QCryptographicHash::Algorithm m_admPwMethod;
+    static quint32 m_admPwRounds;
+    static quint8 m_admPwMinlength;
+
+    static quint32 m_defDomainquota;
+    static quint32 m_defQuota;
+    static quint32 m_defMaxaccounts;
+    static QString m_defLanguage;
+    static QString m_defTimezone;
+
+    static QString m_imapHost;
+    static quint16 m_imapPort;
+    static QString m_imapUser;
+    static QString m_imapPassword;
+    static QAbstractSocket::NetworkLayerProtocol m_imapProtocol;
+    static SkaffariIMAP::EncryptionType m_imapEncryption;
+    static Account::CreateMailbox m_imapCreatemailbox;
+    static bool m_imapDomainasprefix;
+    static bool m_imapFqun;
+};
+
+#endif // SKAFFARICONFIG_H
