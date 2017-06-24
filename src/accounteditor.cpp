cb053857069bc309f3a553b0c6df8a823025140f
Buschmann <kontakt@buschmann23.de>
2017-06-24 18:51:17 +0200

    AccountEditor: fix wrong translation context

--- src/accounteditor.cpp
+++ src/accounteditor.cpp
@@ -210,7 +210,7 @@ void AccountEditor::remove(Context* c)
         } else {
             // this is not an post request, for ajax, we will only allow post
             if (isAjax) {
-                json.insert(QStringLiteral("error_msg"), QJsonValue(c->translate("DomainEditor", "For AJAX requests, this route is only available via POST requests.")));
+                json.insert(QStringLiteral("error_msg"), QJsonValue(c->translate("AccountEditor", "For AJAX requests, this route is only available via POST requests.")));
                 c->response()->setStatus(Response::MethodNotAllowed);
                 c->response()->setHeader(QStringLiteral("Allow"), QStringLiteral("POST"));
             }
