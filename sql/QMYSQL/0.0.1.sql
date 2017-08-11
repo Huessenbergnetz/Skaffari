28a7e8bc5697fbb6439cf38e56e4b8036bfea9fd
Buschmann <kontakt@buschmann23.de>
2017-08-11 13:02:51 +0200

    Add parent_id column to domain table (#5)

--- sql/QMYSQL/0.0.1.sql
+++ sql/QMYSQL/0.0.1.sql
@@ -45,6 +45,7 @@ CREATE TABLE IF NOT EXISTS alias (
 
 CREATE TABLE IF NOT EXISTS domain (
   id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  parent_id int(10) unsigned NOT NULL DEFAULT 0,
   domain_name varchar(255) NOT NULL,
   prefix varchar(50) NOT NULL,
   maxaccounts int(11) NOT NULL DEFAULT 20,
