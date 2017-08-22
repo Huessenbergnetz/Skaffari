9eeb7bdc842e7167da9197ed113e5adc525ec80f
Buschmann <kontakt@buschmann23.de>
2017-08-22 11:28:50 +0200

    Add Select2 and Select2 Bootstrap4 theme.
    See https://select2.github.io
    and https://github.com/select2/select2-bootstrap-theme/issues/60

--- templates/default/gulpfile.js
+++ templates/default/gulpfile.js
@@ -3,13 +3,14 @@ elixir.config.assetsPath = 'assets';
 elixir.config.publicPath = '';
 
 elixir(function(mix) {
-    
+
     mix.sass('style.scss', 'static/css/style.css');
-    
+
     mix.scripts([
         '../../node_modules/jquery/dist/jquery.js',
         '../../node_modules/popper.js/dist/umd/popper.js',
         '../../node_modules/bootstrap/dist/js/bootstrap.js',
+        '../../node_modules/select2/dist/js/select2.js',
         '../../node_modules/queuejax/jquery.qjax.js',
         'stupidtable.js',
         'jquery.filtertable.js',
@@ -22,8 +23,9 @@ elixir(function(mix) {
         'removeaccount.js',
         'removeadmin.js',
         'accountlist.js',
+        'createdomain.js',
         'general.js'
     ], 'static/js/scripts.js');
-    
+
     mix.copy('node_modules/font-awesome/fonts', 'static/fonts');
 });
