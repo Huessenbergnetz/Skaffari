4f589f723171674aebbd6874e7b72c814cab361f
Buschmann <kontakt@buschmann23.de>
2018-02-19 19:59:16 +0100

    QMYSQL db schema: use InnoDB as engine

--- sql/QMYSQL/0.0.1.sql
+++ sql/QMYSQL/0.0.1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS accountuser (
   id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   domain_id int(10) unsigned NOT NULL,
-  username varchar(255)  NOT NULL,
+  username varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   password varchar(255) NOT NULL,
   imap tinyint(1) unsigned NOT NULL DEFAULT 1,
   pop tinyint(1) unsigned NOT NULL DEFAULT 1,
@@ -12,23 +12,23 @@ CREATE TABLE IF NOT EXISTS accountuser (
   updated_at datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   valid_until datetime NOT NULL DEFAULT '2998-12-31 23:59:59',
   pwd_expire datetime NOT NULL DEFAULT '2998-12-31 23:59:59',
-  status tinyint(1) unsigned NOT NULL DEFAULT 0,
+  status tinyint unsigned NOT NULL DEFAULT 0,
   UNIQUE KEY username (username),
   KEY idx_accountuser_domain_id (domain_id)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE IF NOT EXISTS adminuser (
   id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username varchar(255) NOT NULL,
+  username varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   password varchar(255) NOT NULL,
-  type tinyint(1) unsigned NOT NULL DEFAULT 0,
+  type tinyint unsigned NOT NULL DEFAULT 0,
   created_at datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   updated_at datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   valid_until datetime NOT NULL DEFAULT '2998-12-31 23:59:59',
   pwd_expire datetime NOT NULL DEFAULT '2998-12-31 23:59:59',
   UNIQUE KEY username (username)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE IF NOT EXISTS alias (
@@ -38,13 +38,13 @@ CREATE TABLE IF NOT EXISTS alias (
   username varchar(50) NOT NULL DEFAULT '',
   status int(11) NOT NULL DEFAULT '1',
   UNIQUE KEY (alias)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS domain (
   id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   parent_id int(10) unsigned NOT NULL DEFAULT 0,
-  domain_name varchar(255) NOT NULL,
+  domain_name varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   prefix varchar(50) NOT NULL,
   maxaccounts int(11) NOT NULL DEFAULT 20,
   quota bigint(20) unsigned NOT NULL DEFAULT 100000,
@@ -56,18 +56,19 @@ CREATE TABLE IF NOT EXISTS domain (
   accountcount int(10) unsigned NOT NULL DEFAULT 0,
   created_at datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   updated_at datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
+  valid_until datetime NOT NULL DEFAULT '2998-12-31 23:59:59',
   UNIQUE KEY domain_name (domain_name),
   UNIQUE KEY prefix (prefix)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE IF NOT EXISTS folder (
   id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   domain_id int(10) unsigned NOT NULL,
   name varchar(255) NOT NULL,
-  special_use text,
+  special_use text CHARACTER SET latin1,
   KEY idx_folder_domain_id (domain_id)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS domainadmin (
@@ -75,7 +76,7 @@ CREATE TABLE IF NOT EXISTS domainadmin (
   admin_id int(10) unsigned NOT NULL,
   KEY domainadmin_domain_id_idx (domain_id),
   KEY domainadmin_admin_id_idx (admin_id)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE IF NOT EXISTS log (
@@ -88,7 +89,7 @@ CREATE TABLE IF NOT EXISTS log (
   pid varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (id),
   KEY idx_log_user (user)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS settings (
@@ -98,7 +99,7 @@ CREATE TABLE IF NOT EXISTS settings (
   warnlevel tinyint(2) unsigned NOT NULL DEFAULT 90,
   tz varchar(255) NOT NULL DEFAULT 'UTC',
   lang varchar(127) NOT NULL DEFAULT 'en'
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE IF NOT EXISTS virtual (
@@ -107,21 +108,21 @@ CREATE TABLE IF NOT EXISTS virtual (
   username varchar(255) NOT NULL DEFAULT '',
   status int(11) NOT NULL DEFAULT 1,
   KEY alias (alias)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS systeminfo (
   name varchar(127) NOT NULL,
   val longtext NOT NULL
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS options (
   option_id int(11) NOT NULL AUTO_INCREMENT,
   option_name varchar(127) NOT NULL,
-  option_value longtext NOT NULL,
+  option_value longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (option_id),
   UNIQUE KEY option_name (option_name)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO systeminfo (name, val) VALUES ('skaffari_db_version', '0.0.1');
 
