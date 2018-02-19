b31e4f5dc52e828b94ab0b745ad78be0dc4e5a0a
Buschmann <kontakt@buschmann23.de>
2018-02-19 20:18:07 +0100

    AuthStoreSql: add logging category and logging output

--- src/authstoresql.cpp
+++ src/authstoresql.cpp
@@ -22,7 +22,9 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QVariantList>
-#include "../common/global.h"
+#include <QLoggingCategory>
+
+Q_LOGGING_CATEGORY(SK_AUTHSTORE, "skaffari.authstore")
 
 AuthStoreSql::AuthStoreSql(QObject *parent) : AuthenticationStore(parent)
 {
@@ -35,34 +37,41 @@ AuthenticationUser AuthStoreSql::findUser(Context *c, const ParamsMultiMap &user
 
     Q_UNUSED(c);
 
-    const QString username = userinfo[QStringLiteral("username")];
+    const QString username = userinfo.value(QStringLiteral("username"));
 
     QSqlQuery q = CPreparedSqlQueryThread(QStringLiteral("SELECT au.id, au.username, au.password, au.type, se.template, se.maxdisplay, se.warnlevel, se.lang, se.tz FROM adminuser au JOIN settings se ON au.id = se.admin_id WHERE au.username = :username"));
     q.bindValue(QStringLiteral(":username"), username);
 
-    if (q.exec() && q.next()) {
-        user.setId(q.value(0));
-        user.insert(QStringLiteral("username"), q.value(1).toString());
-        user.insert(QStringLiteral("password"), q.value(2).toString());
-        user.insert(QStringLiteral("type"), q.value(3).value<qint16>());
-        user.insert(QStringLiteral("style"), q.value(4).toString());
-        user.insert(QStringLiteral("maxdisplay"), q.value(5).value<quint8>());
-        user.insert(QStringLiteral("warnlevel"), q.value(6).value<quint8>());
-        user.insert(QStringLiteral("lang"), q.value(7).toString());
-        user.insert(QStringLiteral("tz"), q.value(8).toByteArray());
+    if (Q_LIKELY(q.exec())) {
+        if (Q_LIKELY(q.next())) {
+            user.setId(q.value(0));
+            user.insert(QStringLiteral("username"), q.value(1).toString());
+            user.insert(QStringLiteral("password"), q.value(2).toString());
+            user.insert(QStringLiteral("type"), q.value(3).value<qint16>());
+            user.insert(QStringLiteral("style"), q.value(4).toString());
+            user.insert(QStringLiteral("maxdisplay"), q.value(5).value<quint8>());
+            user.insert(QStringLiteral("warnlevel"), q.value(6).value<quint8>());
+            user.insert(QStringLiteral("lang"), q.value(7).toString());
+            user.insert(QStringLiteral("tz"), q.value(8).toByteArray());
+        } else {
+            qCWarning(SK_AUTHSTORE, "Can not find user \"%s\" in the database.", qUtf8Printable(username));
+        }
+    } else {
+        qCCritical(SK_AUTHSTORE, "Failed to execute database query to get user \"%s\" from the database: %s", qUtf8Printable(username), qUtf8Printable(q.lastError().text()));
     }
 
     q = CPreparedSqlQueryThread(QStringLiteral("SELECT domain_id FROM domainadmin WHERE admin_id = :admin_id"));
     q.bindValue(QStringLiteral(":admin_id"), user.id());
 
-    q.exec();
-
-    QVariantList domIds;
-    while (q.next()) {
-        domIds << q.value(0);
+    if (Q_LIKELY(q.exec())) {
+        QVariantList domIds;
+        while (q.next()) {
+            domIds << q.value(0);
+        }
+        user.insert(QStringLiteral("domains"), domIds);
+    } else {
+        qCCritical(SK_AUTHSTORE, "Failed to execute database query to get associated domain IDs for user \"%s\" from the database: %s", qUtf8Printable(username), qUtf8Printable(q.lastError().text()));
     }
-
-    user.insert(QStringLiteral("domains"), domIds);
 
     return user;
 }
