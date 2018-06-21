17e70c8e182e101cd5a190c6f0c03a5305bf2d9f
Buschmann <mf@huessenbergnetz.de>
2018-06-21 08:38:46 +0200

    Improve Content-Security-Policy header

--- src/skaffari.cpp
+++ src/skaffari.cpp
@@ -330,9 +330,9 @@ bool Skaffari::init()
     defaultHeaders().setHeader(QStringLiteral("X-Frame-Options"), QStringLiteral("DENY"));
     defaultHeaders().setHeader(QStringLiteral("X-Content-Type-Options"), QStringLiteral("nosniff"));
     defaultHeaders().setHeader(QStringLiteral("X-XSS-Protection"), QStringLiteral("1; mode=block"));
-    defaultHeaders().setHeader(QStringLiteral("Content-Security-Policy"), QStringLiteral("default-src 'none'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self' data:; connect-src 'self'; base-uri 'self'; form-action 'self';"));
+    defaultHeaders().setHeader(QStringLiteral("Content-Security-Policy"), QStringLiteral("default-src 'none'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self' data:; connect-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; block-all-mixed-content"));
     defaultHeaders().setHeader(QStringLiteral("X-Robots-Tag"), QStringLiteral("none"));
-    defaultHeaders().setHeader(QStringLiteral("Referrer-Policy"), QStringLiteral("no-referrer, strict-origin-when-cross-origin"));
+    defaultHeaders().setHeader(QStringLiteral("Referrer-Policy"), QStringLiteral("same-origin"));
 
     return true;
 }
