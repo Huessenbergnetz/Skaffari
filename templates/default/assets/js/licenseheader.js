61e012b46c09e9e31599ee8247afbdb2ea881995
Buschmann <mf@huessenbergnetz.de>
2021-02-05 21:19:18 +0100

    [Default Template] update bootstrap to version 4.6.0

--- templates/default/assets/js/licenseheader.js
+++ templates/default/assets/js/licenseheader.js
@@ -6,14 +6,14 @@
  * released under MIT license (https://jquery.org/license).
  * Popper.js v1.16.1 (https://popper.js.org/) by Federico Zivolo and contributors,
  * released under MIT license (https://github.com/popperjs/popper-core/blob/v1.16.1/LICENSE.md)
- * Bootstrap v4.5.3 (http://getbootstrap.com) by Twitter Inc.,
- * released under MIT license (https://github.com/twbs/bootstrap/blob/v4.5.3/LICENSE).
+ * Bootstrap v4.6.0 (http://getbootstrap.com) by Twitter Inc.,
+ * released under MIT license (https://github.com/twbs/bootstrap/blob/v4.6.0/LICENSE).
  * qJax v1.5.2 (https://github.com/PortableSheep/qjax) by Michael Gunderson,
  * released under GNU General Public License 2 (https://github.com/PortableSheep/qJax/blob/1.5.2/GPL-LICENSE.txt).
  * Select2 v4.0.13 (https://select2.org/) by Select2 Team,
  * released under MIT license (https://github.com/select2/select2/blob/4.0.13/LICENSE.md).
- * Select2 Bootstrap4 Theme v1.3.4 (https://github.com/ttskch/select2-bootstrap4-theme) by Takashi Kanemoto,
- * released under MIT license (https://github.com/ttskch/select2-bootstrap4-theme/blob/1.3.4/LICENSE).
+ * Select2 Bootstrap4 Theme v1.5.2 (https://github.com/ttskch/select2-bootstrap4-theme) by Takashi Kanemoto,
+ * released under MIT license (https://github.com/ttskch/select2-bootstrap4-theme/blob/1.5.2/LICENSE).
  * Stupid jQuery Table Sort v1.1.3 (http://joequery.github.io/Stupid-Table-Plugin/) by Joseph McCullough,
  * released under MIT license (https://github.com/joequery/Stupid-Table-Plugin/blob/1.1.3/LICENSE).
  * JavaScript Cookie v2.2.1 (https://github.com/js-cookie/js-cookie) by Fagner Brack,
